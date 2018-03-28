e75bea15555f5b9512c9a1abc3b68781d3c56e00
Panmani <mayueen@gmail.com>
2018-03-28 19:35:51 -0400

    server 1.0

--- js/server.js
+++ js/server.js
@@ -4,28 +4,32 @@ var path = require('path');
 
 //=============================== Web page ===============================
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/../index.html'));
 });
 
 app.get('/css/pet_forum.css', function(req, res) {
-    res.sendFile(path.join(__dirname + '/css/pet_forum.css'));
+    res.sendFile(path.join(__dirname + '/../css/pet_forum.css'));
 });
 
 app.get('/js/script.js', function(req, res) {
-    res.sendFile(path.join(__dirname + '/js/script.js'));
+    res.sendFile(path.join(__dirname + '/../js/script.js'));
 });
 
 //=============================== Images ===============================
-app.get('/back.png', function(req, res) {
-    res.sendFile(path.join(__dirname + '/back.png'));
+app.get('/images/back.png', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../images/back.png'));
 });
 
-app.get('/logo.png', function(req, res) {
-    res.sendFile(path.join(__dirname + '/logo.png'));
+app.get('/images/logo.png', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../images/logo.png'));
 });
 
-app.get('/panda.jpg', function(req, res) {
-    res.sendFile(path.join(__dirname + '/panda.jpg'));
+app.get('/images/panda.jpg', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../images/panda.jpg'));
+});
+
+app.get('/favicon.ico', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../images/favicon.ico'));
 });
 
 app.listen(3000);
